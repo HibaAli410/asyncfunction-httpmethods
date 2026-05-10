3b108211aae2399991198e08058f26f1b4b1e151
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-10 17:34:02 +0500

    create folders and place it there functionallity

--- shopify/function.js
+++
@@ -1,8 +0,0 @@
-const draggableItems = document.querySelectorAll('.draggable-container');
-const myDraggable = new Draggable.Sortable(draggableItems,{
-    draggable: '.draggable',
-     mirror: {
-      appendTo: "body",
-      constrainDimensions: true,
-    },
-});

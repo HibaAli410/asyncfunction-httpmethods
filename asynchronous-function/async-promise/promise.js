5c36a6e978b68b7361a81ffdaf7c50ce5c851ffb
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-12 03:26:07 +0500

    2 Promise Functions one is introduction secondone is profite function

--- asynchronous-function/async-promise/promise.js
+++ asynchronous-function/async-promise/promise.js
@@ -1,4 +1,4 @@
-// Creating a new Promise object
+// Creating a new Promise object then take 2 callbacks
 let PromiseObject = new Promise((resolve, reject) => {
   //let condition = true;
   let condition = false
@@ -37,31 +37,72 @@ function bookTicket (name, age) {
   })
 }
 bookTicket('Huzaifa', 12).then(
-  (rider) => {
+  rider => {
     document.getElementById('displayPromise').innerHTML = rider
   },
-  (under18) => {
+  under18 => {
     document.getElementById('displayPromise').innerHTML = under18
   }
 )
-// nested promise
+// nested promise chainging
 function fname (name) {
   return Promise.resolve(name)
 }
 function age (value) {
-  return Promise.resolve(value + ' is years old')
+  return Promise.resolve(value + ' is 30 years old')
 }
 function status (status) {
-  return Promise.resolve(status + 'and you are single')
+  return Promise.resolve(status + 'and you are a Web Developer')
 }
-// promise chaining
 fname('hiba')
   .then(name => {
-    return age(name);
+    return age(name)
   })
-  .then((age) => {
-    return status(age);
+  .then(age => {
+    return status(age)
   })
-  .then((status) => {
-    document.getElementById('displayChainingPromise').innerHTML = status;
+  .then(status => {
+    document.getElementById('displayChainingPromise').innerHTML = status
   })
+
+// Promise chaining
+function add (a, b) {
+  return Promise.resolve(a + b)
+}
+function multiply (value) {
+  return Promise.resolve(value * 2)
+}
+function divide (value) {
+  return Promise.resolve(value / 2)
+}
+add(5, 10)
+  .then(sum => {
+    return multiply(sum)
+  })
+  .then(product => {
+    return divide(product)
+  })
+  .then(result => {
+    document.getElementById('displayChainingPromiseMathematical').innerHTML =
+      'The final result is: ' + result
+  })
+
+// Promise with profit calculation
+function calculateProfit (cost, sell) {
+  return new Promise((resolve, reject) => {
+    if (typeof cost === 'number' && typeof sell === 'number') {
+      const profit = sell - cost
+      resolve(profit)
+    } else {
+      reject('Error: Both cost and sell should be numbers.')
+    }
+  })
+}
+calculateProfit(100, 500).then(
+  (profit) => {
+    document.getElementById('displayProfit').innerHTML = 'Profit: ' + profit;
+  },
+  (error) => {
+    document.getElementById('displayProfit').innerHTML = error;
+  }
+)

29185661a23989be05a8ac190eaa82576d9a8301
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-11 18:12:51 +0500

    assign the values to vaiables and changetime 6000 t0 2000

--- asynchronous-function/todosthrough-api/async-callback/callback.js
+++ asynchronous-function/todosthrough-api/async-callback/callback.js
@@ -1,6 +1,7 @@
-        let value1  ;
-        let value2 ;
-        let value3 ;
+        // Callback function example 1
+        let value1 = "This is value 1"; ;
+        let value2 = "This is value 2"; ;
+        let value3 = "This is value 3"; ;
         setTimeout(()=>{
             value1 = "30";
             value2 = "40";
@@ -8,9 +9,9 @@
             document.getElementById('display1').textContent = value1;
             document.getElementById('display2').textContent = value2;
             document.getElementById('display3').textContent = value3;
-            alert("Values have been updated after 6 seconds");
+            alert("Values have been updated after 2 seconds");
 
-        },6000);
+        },2000);
         function getvalue1(){
             document.getElementById('display1').textContent = value1;
             console.log(value1);
@@ -26,3 +27,17 @@
     getvalue1();
     getvalue2();
     getvalue3();
+
+    // Callback function example 2
+    function fetchData(callback){
+        setTimeout(()=>{
+            const data = "Data fetched successfully!";
+            callback(data);
+        },3000);
+    }
+    function displayData(data){
+        console.log(data);
+        document.getElementById('dataDisplay').textContent = data;
+        
+    }
+    fetchData(displayData);

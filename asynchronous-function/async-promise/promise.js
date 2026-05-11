67cbc5adea5e347f874a9a46997a374b0e40600f
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-12 02:09:17 +0500

    BookTicket Promise

--- asynchronous-function/async-promise/promise.js
+++ asynchronous-function/async-promise/promise.js
@@ -1,21 +1,46 @@
 // Creating a new Promise object
-let PromiseObject = new Promise((resolve,reject)=>{
-    //let condition = true;
-    let condition = false;
-    if(condition){
-        resolve("Promise is resolved successfully");
-    }
-    else{
-        reject("Promise is rejected");
-    }
+let PromiseObject = new Promise((resolve, reject) => {
+  //let condition = true;
+  let condition = false
+  if (condition) {
+    resolve('Promise is resolved successfully')
+  } else {
+    reject('Promise is rejected')
+  }
 })
 
-PromiseObject.then((message)=>{
-    document.getElementById("displayPromise").innerHTML = message;
-    console.log(message);
-},(Err)=>{
-    document.getElementById("displayPromise").innerHTML = Err;
-    console.log(Err);
-})
+PromiseObject.then(
+  message => {
+    document.getElementById('displayPromise').innerHTML = message
+    console.log(message)
+  },
+  Err => {
+    document.getElementById('displayPromise').innerHTML = Err
+    console.log(Err)
+  }
+)
 
-    
+//Creating a zoo ticket booking system using Promise
+function bookTicket (name, age) {
+  return new Promise((bookingResolve, bookingReject) => {
+    if (age >= 18) {
+      bookingResolve(
+        'Congratulations ' + name + ' You Can Enjoy Our RollerCoaster Ride'
+      )
+    } else {
+      bookingReject(
+        "' Sorry '" +
+          name +
+          " You are Under 18 So you Can't enjoy our rollercoaster Ride "
+      )
+    }
+  })
+}
+bookTicket('Huzaifa', 12).then(
+  (rider) => {
+    document.getElementById('displayPromise').innerHTML = rider
+  },
+  (under18) => {
+    document.getElementById('displayPromise').innerHTML = under18
+  }
+)

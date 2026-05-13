eba060b919537f50861f4dbc7397ff80ad03c30e
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-13 20:45:03 +0500

    export name function

---
+++ js-modules/name.js
@@ -0,0 +1,6 @@
+export function naMe (fname, lname) {
+  const fname = document.createElement('h1')
+  const lname = document.createElement('h1')
+  const fullName = fname + lname
+  return fullName
+}
